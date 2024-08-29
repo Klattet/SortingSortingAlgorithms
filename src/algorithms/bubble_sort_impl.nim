f6d43461aebcc39ceeff68a746f59041bfc392cc
Klattet <lyngvollan@protonmail.com>
2024-08-30 01:33:49 +0200

    Testing for loop vs while loop

--- src/algorithms/bubble_sort_impl.nim
+++ src/algorithms/bubble_sort_impl.nim
@@ -1,10 +1,20 @@
 
 proc naive_bubble_sort*(sequence: var seq[int]): void =
     let size = sequence.len
-    for i in 1 ..< size:
-        for j in 1 .. size - i:
+    
+    var i = 0
+    while i < size:
+        var j = 1
+        while j < size - i:
             if sequence[j - 1] > sequence[j]:
                 swap(sequence[j - 1],sequence[j])
+            j += 1
+        i += 1
+    
+    #for i in 1 ..< size:
+    #    for j in 1 .. size - i:
+    #        if sequence[j - 1] > sequence[j]:
+    #            swap(sequence[j - 1],sequence[j])
 
 proc optimised_bubble_sort*(sequence: var seq[int]): void =
     let size = sequence.len
